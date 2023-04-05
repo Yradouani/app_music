82165153ba336f85117b1e4bc50b58f92baf6416
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-04-05 15:27:53 +0200

    hundred tracks

---
+++ public/assets/js/discovery.js
@@ -0,0 +1,28 @@
+let allTracks = document.getElementById("allTracks");
+let tableauTop100 = "";
+
+
+
+for(let i = 0; i < 100; i++) {
+    tableauTop100 += `
+                        <td style="width:6%">
+                            <img id="albumCover" src="assets/images/albumCover.jpg" alt="albumImg">
+                        </td>
+                        <td style="width:1%">
+                            1.
+                        </td>
+                        <td style="width:36%">
+                            Flowers
+                        </td>
+                        <td>
+                            <i id="heart" class="fa-regular fa-heart"></i>
+                        </td>
+                        <td>
+                            <i id="plus" class="fa-solid fa-plus"></i>
+                        </td>
+                        <td>Miley Cyrus</td>
+                        <td>Flowers</td>
+                        </tr>`;
+}
+
+allTracks.innerHTML = tableauTop100;
