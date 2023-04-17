d2b98d2235f1a4a14640e3660a1a6a2108bdb4bc
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-04-17 16:06:44 +0200

    first push

--- public/assets/js/discovery.js
+++ public/assets/js/discovery.js
@@ -9,150 +9,113 @@ let search = document.getElementById("search");
 let newGenreTitle = "";
 let tableauTop = "";
 
-for(let i = 0; i < genreButton.length; i++) {
+
+
+fn_top100();
+
+for (let i = 0; i < genreButton.length; i++) {
     genreButton[i].addEventListener("click", () => fn_loadGenre(i));
 }
 
 searchIcon.addEventListener("click", () => fn_search());
 
-function fn_top100() {
-    // au chargement de la page charge le top 100 avec l'api
-    
-    const options = {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
-    };
-    getTopTracks()
-
-<<<<<<< HEAD
-    fetch('https://api.deezer.com/playlist/1109890291', options)
-=======
-    function getTopTracks() {
-    fetch('https://deezerdevs-deezer.p.rapidapi.com/playlist/1109890291', options)
->>>>>>> b1e730070f221ba69cc8f5cd41a463028e46c646
-        .then(response => response.json())
-        .then(response => displayTopTracks(response))
-        .catch(err => console.error(err));
-}
-    
-
-function displayTopTracks(response){
-    console.log(response);
-    if(response.error){
-        console.log(response);
-        getTopTracks();
-    }else{
-    
-        trackId = response.tracks.data[0].id ;
-    for(let i = 0; i < 100; i++) {
-            tableauTop += `<tr class='track-container' id="${ response.tracks.data[i].id }" onclick=changeMusicInPlayer(this)>
-                                <td style="width:6%">
-                                    <img id="albumCover" src="${ response.tracks.data[i].album.cover_big }" alt="albumImg">
-                                </td>
-                                <td style="width:1%">
-                                    ${ i + 1 }.
-                                </td>
-                                <td style="width:36%">
-                                    ${ response.tracks.data[i].title }
-                                </td>
-                                <td>
-                                    <i id="heart" class="fa-regular fa-heart"></i>
-                                </td>
-                                <td>
-                                    <i id="plus" class="fa-solid fa-plus"></i>
-                                </td>
-                                <td>
-                                    ${ response.tracks.data[i].artist.name }
-                                </td>
-                                <td>
-                                    ${ response.tracks.data[i].album.title }
-                                </td>
-                            </tr>`;
-    }
-    allTracks.innerHTML = tableauTop;
-}
-}
-}
-
-fn_top100();
 
 
-function fn_loadGenre(i) {
-    titleTab.innerHTML = "Top " + genreButton[i].innerHTML;
-    idGenre = genreButton[i].getAttribute("name");
+function getTracks(url, fnName) {
 
-    // console.log("hello");
-    console.log(idGenre);
-
-    allTracks.innerHTML = "";
-    tableauTop = "";
-
-    const options = {
+    fetch('https://deezerdevs-deezer.p.rapidapi.com/' + url, {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'b824698b76mshc29d02afeecec35p15959cjsn24db764eeaad',
             'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
         }
-    };
-    getTrackByGenre(idGenre)
-function getTrackByGenre(idGenre) {
-    fetch('https://deezerdevs-deezer.p.rapidapi.com/playlist/' + idGenre, options)
+    })
         .then(response => response.json())
-        .then(response => displayTrackByGenre(response, idGenre))
+        .then(response => fnName(response, url, fnName))
         .catch(err => console.error(err));
-
 }
-  
-        function displayTrackByGenre(response, idGenre) {
-            if(response.error){
-                getTrackByGenre(idGenre)
-            }{
-                for(let i = 0; i < 50; i++) {
-                    tableauTop += `  <tr class='track-container' id="${ response.tracks.data[i].id }" onclick=changeMusicInPlayer(this)>
-                                        <td style="width:6%">
-                                            <img id="albumCover" src="${ response.tracks.data[i].album.cover_big }" alt="albumImg">
-                                        </td>
-                                        <td style="width:1%">
-                                            ${ i + 1 }.
-                                        </td>
-                                        <td style="width:36%">
-                                            ${ response.tracks.data[i].title }
-                                        </td>
-                                        <td>
-                                            <i id="heart" class="fa-regular fa-heart"></i>
-                                        </td>
-                                        <td>
-                                            <i id="plus" class="fa-solid fa-plus"></i>
-                                        </td>
-                                        <td>
-                                            ${ response.tracks.data[i].artist.name }
-                                        </td>
-                                        <td>
-                                            ${ response.tracks.data[i].album.title }
-                                        </td>
-                                    </tr>`;
-            }
-            allTracks.innerHTML = tableauTop;
-            }
-            
+
+
+
+function createTable(response, tableLength, tracks) {
+
+    for (let i = 0; i < tableLength; i++) {
+        tableauTop += `<tr class='track-container' id="${(tracks) ? response.tracks.data[i].id : response.data[i].id}" onclick=changeMusicInPlayer(this)>
+                            <td style="width:6%">
+                                <img id="albumCover" src="${(tracks) ? response.tracks.data[i].album.cover_big : response.data[i].album.cover_big}" alt="albumImg">
+                            </td>
+                            <td style="width:1%">
+                                ${i + 1}.
+                            </td>
+                            <td style="width:36%">
+                                ${(tracks) ? response.tracks.data[i].title : response.data[i].title}
+                            </td>
+                            <td>
+                                <i id="heart" class="fa-regular fa-heart"></i>
+                            </td>
+                            <td>
+                                <i id="plus" class="fa-solid fa-plus"></i>
+                            </td>
+                            <td>
+                                ${(tracks) ? response.tracks.data[i].artist.name : response.data[i].artist.name}
+                            </td>
+                            <td>
+                                ${(tracks) ? response.tracks.data[i].album.title : response.data[i].album.title}
+                            </td>
+                        </tr>`;
+    }
+    allTracks.innerHTML = tableauTop;
+}
+
+function fn_top100() {
+    // au chargement de la page charge le top 100 avec l'api
+
+    let url = "playlist/1109890291";
+    let fnName = displayTopTracks;
+
+    getTracks(url, fnName);
+
+    function displayTopTracks(response, url, fnName) {
+        console.log(response);
+        if (response.error) {
+            console.log("error" + response);
+            getTracks(url, fnName);
+        } else {
+
+            let tabLength = 100;
+            let tracks = true;
+
+            createTable(response, tabLength, tracks);
         }
+    }
 }
 
-function changeMusicInPlayer(track){
-    trackId = track.id;
-    getTrack(trackId)
+function fn_loadGenre(i) {
+    titleTab.innerHTML = "Top " + genreButton[i].innerHTML;
+    idGenre = genreButton[i].getAttribute("name");
 
-    sound.stop();
-    startStopBtn.innerHTML = "<i class='fa-solid fa-play'></i>"; 
-    elapsed = 0;
-    inputPlayer.value = elapsed; 
-    clearInterval(intervalId);
+    let url = "playlist/" + idGenre;
+    let fnName = displayTrackByGenre;
+
+    console.log(url);
+
+    allTracks.innerHTML = "";
+    tableauTop = "";
+
+    getTracks(url, fnName);
+
+    function displayTrackByGenre(response, url, fnName) {
+        console.log(response);
+        if (response.error) {
+            getTracks(url, fnName);
+        } else {
+            let tabLength = 50;
+            let tracks = true;
+
+            createTable(response, tabLength, tracks);
+        }
+    }
 }
-
 
 function fn_search() {
     titleTab.style.display = "none";
@@ -160,75 +123,37 @@ function fn_search() {
     divSwiper.style.display = "none";
     divSearch.style.marginBottom = "40px";
 
-    console.log(search.value);
+    let url = 'search?q=' + search.value;
+    let fnName = displaySearchResult;
+
+    console.log(url);
 
     allTracks.innerHTML = "";
     tableauTop = "";
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'b824698b76mshc29d02afeecec35p15959cjsn24db764eeaad',
-            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    getTracks(url, fnName);
+
+    function displaySearchResult(response, url, fnName) {
+        if (response.error) {
+            getTracks(url, fnName);
+        } else {
+            let tabLength = 25;
+            let tracks = false;
+
+            createTable(response, tabLength, tracks);
         }
-    };
-
-    getSearchResult(search);
-
-    function getSearchResult(search){
-        fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=' + search.value, options)
-            .then(response => response.json())
-            .then(response => displaySearchResult(response, search))
-            .catch(err => console.error(err));
-    }
-
-    function displaySearchResult(response, search) {
-        if(response.error){
-            getSearchResult(search)
-        }else{
-            for(let i = 0; i < 25; i++) {
-                tableauTop += `  <tr class='track-container' id="${ response.tracks.data[i].id }">
-                                    <td style="width:6%">
-                                        <img id="albumCover" src="${ response.data[i].album.cover_big }" alt="albumImg">
-                                    </td>
-                                    <td style="width:1%">
-                                        ${ i + 1 }.
-                                    </td>
-                                    <td style="width:36%">
-                                        ${ response.data[i].title }
-                                    </td>
-                                    <td>
-                                        <i id="heart" class="fa-regular fa-heart"></i>
-                                    </td>
-                                    <td>
-                                        <i id="plus" class="fa-solid fa-plus"></i>
-                                    </td>
-                                    <td>
-                                        ${ response.data[i].artist.name }
-                                    </td>
-                                    <td>
-                                        ${ response.data[i].album.title }
-                                    </td>
-                                </tr>`;
-        }
-        allTracks.innerHTML = tableauTop;
     }
 }
 
-}
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'b824698b76mshc29d02afeecec35p15959cjsn24db764eeaad',
-// 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-// 	}
-// };
+//-------Music Player------//
 
-// fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem', options)
-// 	.then(response => response.json())
-// 	.then(response => {
-//         for(let i = 0; i < 1 ;i++) {
-//             console.log(response.data);
-//         }
-//     })
-// 	.catch(err => console.error(err));
+function changeMusicInPlayer(track) {
+    trackId = track.id;
+    getTrack(trackId)
+
+    sound.stop();
+    startStopBtn.innerHTML = "<i class='fa-solid fa-play'></i>";
+    elapsed = 0;
+    inputPlayer.value = elapsed;
+    clearInterval(intervalId);
+}
