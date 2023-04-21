a384c0bab1067f82daca38a2d3eb24be11beea39
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-04-21 15:43:26 +0200

    wip display favoriteList

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,37 +2,78 @@
 
 namespace App\Controller;
 
-use App\Entity\Favorite;
 use App\Entity\User;
+use App\Entity\Favorite;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FavoriteController extends AbstractController
 {
     #[Route('/favorite', name: 'favorite.index')]
-    public function index(SessionInterface $session): Response
+    public function index(SessionInterface $session, EntityManagerInterface $manager): Response
     {
         $idUser = $session->get('idUser');
         if (isset($idUser)) {
-            return $this->render('favorite/favorite.html.twig', [
-                'controller_name' => 'FavoriteController',
-            ]);
+
+            $userRepository = $manager->getRepository(User::class);
+            $user = $userRepository->find($idUser);
+
+            $favoriteRepository = $manager->getRepository(Favorite::class);
+            $favoriteList = $favoriteRepository->findBy(['id_user' => $user]);
+
+            // var_dump($favoriteList);
+            // echo $favoriteList[0]->getIdTrack();
+
+            // $favorite = $favoriteList[0];
+            // $idTrack = $favorite->getIdTrack();
+            // echo $idTrack;
+            
+            $responseTrack = [];
+
+            for ($i = 0; $i < count($favoriteList); $i++) {
+                $idTrack = $favoriteList[$i]->getIdTrack();
+                $url = 'https://api.deezer.com/track/' . $idTrack;
+                $option = [
+                    'http' => [
+                        'method' => 'GET'
+                    ]
+                ];
+                $context = stream_context_create($option);
+                $response = file_get_contents($url, false, $context);
+                $responseTrack[$i] = json_decode($response, true);
+            }
+
+            if ($responseTrack === false) {
+                $errorGetContent = "Une erreur s'est produite au chargement des Favoris, veuillez recharger la page.";
+                return $this->render('favorite/favorite.html.twig', [
+                    'controller_name' => 'FavoriteController',
+                    'userNum' => $user,
+                    'errorGetContent' => $errorGetContent,
+                ]);
+            } else {
+                return $this->render('favorite/favorite.html.twig', [
+                    'controller_name' => 'FavoriteController',
+                    'userNum' => $user,
+                    'responseTrack' => $responseTrack,
+                ]);
+            }
         } else {
             return $this->redirectToRoute('home.index');
         }
     }
 
-    #[Route('/addFavorite', name: 'addFavorite.index', methods:['POST'])]
+    #[Route('/addFavorite', name: 'addFavorite.index', methods: ['POST'])]
     public function addFavorite(Request $request, EntityManagerInterface $manager, SessionInterface $session): JsonResponse
     {
 
         $data = json_decode($request->getContent(), true);
-        $songId= $data['songId'];
+        $songId = $data['songId'];
 
         $idUser = $session->get('idUser');
 
