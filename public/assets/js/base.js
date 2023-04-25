fef01b10a897f720b841633d59cd450936a89d78
Yradouani <yasmine.radouani@outlook.fr>
2023-04-25 15:37:24 +0200

    responsive du pseudo

--- public/assets/js/base.js
+++ public/assets/js/base.js
@@ -370,14 +370,6 @@ switch (location.pathname) {
 // -------------------------------Player add in playlist------------------
 
 //------------------Get id Track in player------------------------
-let idTrackInPlayer
-setTimeout(() => {
-  let footer = document.querySelector(".myfooter");
-  idTrackInPlayer = (footer.id).split('-');
-  idTrackInPlayer = idTrackInPlayer[1];
-  console.log(idTrackInPlayer)
-}, 2000);
-
 let playerBtnAddPlaylist = document.querySelector(".playlist-btn");
 let bgDark = document.querySelector("#bg-dark");
 let modal = document.querySelector("#modal");
@@ -385,6 +377,10 @@ let modal = document.querySelector("#modal");
 playerBtnAddPlaylist.addEventListener("click", () => {
   modal.style.display = "block";
   bgDark.style.display = "block";
+  let footer = document.querySelector(".myfooter");
+  let idTrackInPlayer = (footer.id).split('-');
+  idTrackInPlayer = idTrackInPlayer[1];
+  console.log(idTrackInPlayer)
   document.getElementById("track_id_input").value = idTrackInPlayer;
 
   document.onmouseup = (e) => {
